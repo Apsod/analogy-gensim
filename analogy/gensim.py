1149bcc53e36307687ee840501cc7cae93c6eaf6
Amaru Cuba Gyllensten <amaru.cuba.gyllensten@sics.se>
2018-04-03 18:31:26 +0200

    fixed batched queries, for efficiency

--- analogy/gensim.py
+++ analogy/gensim.py
@@ -1,4 +1,37 @@
-from analogy.wrapper import Model
+from analogy.wrapper import Wrapper
+import numpy
+from gensim.models import KeyedVectors
 
-class Gensim(Model):
-    pass
+
+class Gensim(Wrapper):
+    """
+    An example of the analogy-test wrapper that works for gensims keyed vectors.
+    """
+    def __init__(self, model):
+        self.model = model
+        self.w2i = {w: i for i, w in enumerate(model.index2word)}
+        self.model.init_sims(replace=True)
+
+    def analogies(self, queries):
+        m = self.model.syn0norm
+        aa, bb, xx = zip(*queries)
+        ai = [self.w2i[a] for a in aa]
+        bi = [self.w2i[b] for b in bb]
+        xi = [self.w2i[x] for x in xx]
+        v = m[bi] + m[xi] - m[ai]
+        sims = v @ m.T
+        for i in range(len(aa)):
+            sims[i, ai[i]] = float('-inf')
+            sims[i, bi[i]] = float('-inf')
+            sims[i, xi[i]] = float('-inf')
+
+        return [self.model.index2word[i] for i in numpy.argmax(sims, axis=1)]
+
+    def members(self, items):
+        return [i in self.model for i in items]
+
+    @staticmethod
+    def load(path):
+        return Gensim(KeyedVectors.load(path))
+
+
--- setup.py
+++ setup.py
@@ -4,9 +4,7 @@ setup(
     name='analogy-gensim',
     version='0.0.1',
 
-    setup_requires=['analogy'],
-    install_requires=['analogy'],
-
+    requires=['analogy', 'gensim', 'numpy'],
     package_dir={'analogy': 'analogy'},
     packages=['analogy'],
 
